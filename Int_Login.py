f38b051f3b0daf45ad816757c649542b5b718081
Ximena-Margarita-Hernandez-Gatica <hernandezxgatica@gmail.com>
2025-07-30 12:09:28 -0600

    Primera Versión

---
+++ Int_Login.py
@@ -0,0 +1,52 @@
+import flet as ft
+import principal as pr
+#import airtable as at#
+
+#Función principal
+def main (page: ft.Page):
+
+    def ingresar(e: ft.ControlEvent):
+        page.clean()
+        pr.main (page)
+
+    #*Configuración de la pagina*
+    page.theme_mode = "light" #Si queremos el modo oscuro escribiriamos "dark" en vez de "light"
+    page.horizontal_alignment = "center" #Centrar(tambien se puede poner vertical)
+    page.title = "Inicio de sesión" #Poner titulo a la pagina (se veria en lo que seria la pestaña de la pagina)
+    page.window.width = 800 #Ancho de la pagina
+    page.window.height = 600 #Altura de la pagina
+    page.fonts = {
+        "Kanit": "https://raw.githubusercontent.com/google/fonts/master/ofl/kanit/Kanit-Bold.ttf",
+    }
+
+    #*Componentes de la pagina*
+    logo = ft.Icon ("person", size=60, color=ft.Colors.RED_100) #Poner el logo,configurar su tamaño y color (el icono se busca en "Flet Icons browser" y se escribe el nombre del icono [solo el nombre qu e va despues del icon.])
+    #txt_bienvenido = ft.Text ("Bienvenid@", size=40,color=ft.Colors.RED_100) #Cartelito de bienvenida
+    page.appbar = ft.AppBar (
+        title= ft.Text ("Bienvenid@",font_family="Kanit"),
+        center_title=True,
+        leading= ft.Icon ("person_add"),
+        color = "black",
+        bgcolor= ft.Colors.RED_100,
+    )
+    txt_usuario = ft.TextField (label="Username/Correo", width=250) #Poner el campo de texto en donde el usuario agregara su nombre/correo, ademas se configura ancho y largo de ese campo de texto
+    txt_contra = ft.TextField (label="Contraseña", password=True,can_reveal_password=True,width=250) #Poner el campo de texto en donde el usuario agregara su contraseña, ademas se configura ancho y largo de ese campo de texto y ocultar la contraseña (can_reveal_password=True para el ojito que permite ocultar y ver la pagina)
+    btn_login = ft.FilledButton ( #Si la linea es muy larga se puede ordenar como aqui
+        "Iniciar sesión", 
+        icon=ft.Icons.LOGIN,
+        width=300,
+        color="black",#Color de la letra del boton
+        bgcolor=ft.Colors.RED_100,#Color del boton
+        on_click = ingresar # Ejecutar un funcion que valide al usuario (que los campos no este vacios,etc)
+        ) #Boton de inicion de sesión junto con un icono
+    
+    #*Agregar icono,caja del nombre/correo, caja de correo y todos los demas componentes a la pagina (que se puedan mostrar)*
+    page.add(logo,txt_usuario,txt_contra,btn_login) #Es importante agregarlar en el orden en que queremos que se muestre (en base a nuestro diseño)
+#
+
+    #*Actulizar la pagina*
+    page.update() #Importante para que se vallan viendo los cambios
+
+
+#Inicializar la aplicación
+ft.app (target = main)
---
+++ principal.py
@@ -0,0 +1,71 @@
+import flet as ft
+import Interfaz_RegiDatos as IRD
+import ConsultasUsu as CS
+import alta_usuario as AU
+
+def main (page: ft.Page):
+    #Configuración de la pagina
+    page.theme_mode = "light" 
+    page.horizontal_alignment = "center" 
+    page.title = "Menú Principal" 
+    page.window.width = 800 
+    page.window.height = 600 
+    page.fonts = {
+        "Kanit": "https://raw.githubusercontent.com/google/fonts/master/ofl/kanit/Kanit-Bold.ttf",
+    }
+    #Funciones para abrir las otras paginas
+    def mostrar_registrodatos(e: ft.ControlEvent):
+        page.clean()
+        IRD.main(page)
+    def mostrar_consultasUsu (e: ft.ControlEvent):
+        page.clean()
+        CS.main(page)
+    def mostrar_altaUsu(e: ft.ControlEvent):
+        page.clean()
+        AU.main(page)
+    #Componentes de la pagina
+    page.appbar = ft.AppBar (
+        title= ft.Text ("Sistema de Gestión de Bionergías",font_family="Kanit",size=30),
+        center_title=True,
+        leading= ft.Icon ("ENERGY_SAVINGS_LEAF"),
+        color = "black",
+        bgcolor= ft.Colors.RED_100,
+    )
+    
+    btn_registro = ft.FilledButton (
+        text= "Registro de bioenergias",
+        color="black",
+        bgcolor= ft.Colors.RED_100,
+        icon= "ENERGY_SAVINGS_LEAF_OUTLINED",
+        icon_color = "black",
+        on_click =mostrar_registrodatos
+    )
+    btn_consulta = ft.FilledButton (
+        text= "Consulta de usuarios",
+        color="black",
+        bgcolor= ft.Colors.RED_100,
+        icon= "PERSON_SEARCH",
+        icon_color = "black",
+        on_click=mostrar_consultasUsu
+    )
+    btn_alta = ft.FilledButton (
+        text= "Registro de usuarios (altas)",
+        color="black",
+        bgcolor= ft.Colors.RED_100,
+        icon= "PERSON_ADD",
+        icon_color = "black",
+        on_click=mostrar_altaUsu
+    )
+
+    """
+    btn_registro = ft.ElevatedButton ("Registro de bioenergias", on_click =mostrar_registrodatos )
+    btn_consulta = ft.ElevatedButton ("Consulta de usuarios", on_click=mostrar_consultasUsu)
+    btn_alta = ft.ElevatedButton ("Registro de usuarios (altas)", on_click=mostrar_altaUsu)
+    """
+
+    #Añadir a la pagina y actualizar
+    page.add(btn_registro,btn_consulta,btn_alta) 
+    page.update() 
+
+if __name__ == "__main__":
+    ft.app(target=main)
