448ab3d4b7d537df755a46c0aa898b74df4e44c8
Ximena-Margarita-Hernandez-Gatica <hernandezxgatica@gmail.com>
2025-07-30 23:09:38 -0600

    Archivos iniciales del sistema

---
+++ principal.py
@@ -0,0 +1,23 @@
+import flet as ft
+
+def main (page: ft.Page):
+    #Configuración de la pagina
+    page.theme_mode = "light" 
+    page.title = "Menú Principal" 
+    page.appbar = ft.AppBar (
+        title= ft.Text ("Sistema de Gestión de Bionergías",font_family="Kanit",size=30),
+        center_title=True,
+        leading= ft.Icon ("ENERGY_SAVINGS_LEAF"),
+        color = "black",
+        bgcolor= ft.Colors.RED_100,
+    )
+    #Componentes de la pagina
+    btn_registro = ft.ElevatedButton ("Registro")
+    btn_consulta = ft.ElevatedButton("Consulta")
+
+    #Añadir a la pagina y actualizar
+    page.add(btn_registro,btn_consulta) 
+    page.update() 
+
+if __name__ == "__main__":
+    ft.app(target=main,view= ft.AppView.WEB_BROWSER)
